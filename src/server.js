fd490badadb84d6c94788fa8c808a6de77718e75
Ali <dev.aliakbar@gmail.com>
2020-10-14 13:09:48 +0530

    Init

---
+++ src/server.js
@@ -0,0 +1,22 @@
+import { GraphQLServer, PubSub } from "graphql-yoga";
+
+import { resolvers, fragmentReplacements } from "./resolvers/index";
+
+import prisma from "./prisma";
+
+const pubSub = new PubSub();
+
+const server = new GraphQLServer({
+  typeDefs: "./src/schema.graphql",
+  resolvers,
+  context(request) {
+    return {
+      pubSub,
+      prisma,
+      request,
+    };
+  },
+  fragmentReplacements,
+});
+
+export { server as default };
